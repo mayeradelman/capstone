5cb5ee10c3ed8a248e22970b37272bcf037c3ab3
mayeradelman <mayeradelman@gmail.com>
2023-01-29 23:26:57 -0500

    updated

--- Mayer/endpoint_test.py
+++ Mayer/endpoint_test.py
@@ -1,7 +1,12 @@
 import requests
 
-url = 'https://kqua4jnymh.execute-api.us-east-1.amazonaws.com/stage-1/classify'
 
-# send basic post request
-r = requests.post(url, )
+def post_image():
+    url = 'https://kqua4jnymh.execute-api.us-east-1.amazonaws.com/stage-1/classify'
+
+    # send basic post request
+    r = requests.post(url, files={'logo': open('foo.png','rb')})
+    return r
+
+r = post_image()
 print(r.text)
